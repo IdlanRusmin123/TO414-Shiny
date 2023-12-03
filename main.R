5c8eee3d329085dce226f7db3e17377d0bb205e5
IdlanRusmin123 <mrusmin@umich.edu>
2023-12-03 03:59:19 -0500

    Changed main

--- main.R
+++ main.R
@@ -6,12 +6,21 @@ library(randomForest)
 library(kernlab)
 library(rpart)
 
-vals <- read.csv("players.csv")
+lin_m <- readRDS(gzcon(url("https://raw.githubusercontent.com/IdlanRusmin123/TO414-Shiny/master/Linear_Reg.rds")))
+tree_m <- readRDS(gzcon(url("https://raw.githubusercontent.com/IdlanRusmin123/TO414-Shiny/master/Reg_Tree.rds")))
+rf_m <- readRDS(gzcon(url("https://raw.githubusercontent.com/IdlanRusmin123/TO414-Shiny/master/Rand_For.rds")))
+ann_m <- readRDS(gzcon(url("https://raw.githubusercontent.com/IdlanRusmin123/TO414-Shiny/master/ANN.rds")))
+svm_m <- readRDS(gzcon(url("https://raw.githubusercontent.com/IdlanRusmin123/TO414-Shiny/master/SVM.rds")))
+knn_m <- readRDS(gzcon(url("https://raw.githubusercontent.com/IdlanRusmin123/TO414-Shiny/master/KNN.rds")))
+stacked_m <- readRDS(gzcon(url("https://raw.githubusercontent.com/IdlanRusmin123/TO414-Shiny/master/Stacked.rds")))
+
+vals <- read.csv("https://raw.githubusercontent.com/IdlanRusmin123/TO414-Shiny/master/players.csv")
 
 # mapped_players <- player_dictionary_mapping()
 
 # bio <- tm_player_bio(player_url = "https://www.transfermarkt.com/ollie-watkins/profil/spieler/324358")
 
+fun1 <- function(fbref) {
 standard <- fb_player_season_stats(fbref, stat_type = 'standard', time_pause = 0)
 shooting <- fb_player_season_stats(fbref, stat_type = 'shooting', time_pause = 0)
 gca <- fb_player_season_stats(fbref, stat_type = 'gca', time_pause = 0)
@@ -64,9 +73,32 @@ test_data <- data.frame(goals_per90 = total_goals/total_mins_per_90,
                         passes_pct = (passes_completed/passes_attempted) * 100,
                         touches_att_3rd = sum(possession[which(possession$Season == "2022-2023"), "Att 3rd_Touches"]),
                         value = as.numeric(vals[which(vals$name == standard$player_name[1]), "market_value_in_eur"])
-)
+                        )
 
 if(any(is.na(test_data))) {stop("Missing predictors detected. Please choose another player.")}
+
+table_out1 <<- data.frame(Name = standard$player_name[1],
+                         Age = test_data$age[1],
+                         Height_in_cm = test_data$height[1],
+                         Foot = test_data$foot[1],
+                         Goals_Per_90 = test_data$goals_per90[1],
+                         Assists_Per_90 = test_data$assists_per90[1],
+                         Penalty_Goals_Per_90 = test_data$goals_pens_per90[1],
+                         Expected_Goals_Per_90 = test_data$xg_per90[1],
+                         Expected_Assists_Per_90 = test_data$xa_per90[1],
+                         Shot_Creating_Action_Per_90 = test_data$sca_per90[1]
+)
+
+table_out2 <<- data.frame(Non_Penalty_Expected_Goals_Per_90 = test_data$npxg_per90[1],
+                         Shots_Per_90 = test_data$shots_total_per90[1],
+                         Shots_On_Target_Per_90 = test_data$shots_on_target_per90[1],
+                         Goal_Creating_Action_Per_90 = test_data$gca_per90[1],
+                         Minutes_Played = test_data$minutes[1],
+                         Games_Played = test_data$games[1],
+                         Games_Starts = test_data$games_starts[1],
+                         Pass_Completion_Percentage = test_data$passes_pct[1],
+                         Touches_Attacking_3rd = test_data$touches_att_3rd[1]
+)
 
 testmm <- test_data
 testmm <- add_column(testmm, foot4 = 0, .after = "foot")
@@ -104,5 +136,6 @@ stacked_p <- predict(stacked_m, stacked)
 
 options(scipen = 100, digits = 4)
 
-prediction <- as.numeric(stacked_p)
-given <- stacked$value
+pred <<- as.numeric(stacked_p)
+given <<- stacked$value
+}
